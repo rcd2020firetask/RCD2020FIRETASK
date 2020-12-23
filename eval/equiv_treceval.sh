f4df39c607ea68d14b97bad5f0c655c1b42d3ce9
Debasis-Ganguly1 <debforit@Debasiss-MacBook-Pro.local>
2020-12-23 18:53:17 +0000

    fixed bug

--- eval/equiv_treceval.sh
+++ eval/equiv_treceval.sh
@@ -2,19 +2,26 @@
   
 if [ $# -lt 2 ]
 then
-    echo "usage: $0 <qrel> <res>"
+    echo "usage: $0 <qrels> <res>"
     exit
 fi
 
 QREL=$1
 RESFILE=$2
-MERGED_QREL=$QREL.merged
 
-awk 'FNR==NR \
-{repqid[$1]=$0; next} \
-{if ($4>0) reldocs[$1]=reldocs[$1]" "$3} \
-END {for (i in repqid) { split(repqid[i], qids, " "); for (j in qids) merged_rels=merged_rels" "reldocs[qids[j]]; n=split(merged_rels,docids," "); \
-for (k=1;k<=n;k++) uniqdocs[docids[k]]=1; \
-for (j in qids) { for (uniqdoc in uniqdocs) printf("%s\tQ0\t%s\t1\n", qids[j], uniqdoc); } } }' equiv.txt $QREL > $MERGED_QREL
+awk 'FNR==NR { repqid[$1]=$0; next } { if ($4>0) { reldocs[$1] = reldocs[$1]" "$3;} } END { \
+for (i in repqid) { \
+	merged_rels=""; \
+	split(repqid[i], qids, " "); \
+	for (j in qids) \
+		merged_rels=merged_rels" "reldocs[qids[j]]; \
+	n=split(merged_rels,docids," "); \
+	for (k=1;k<=n;k++) \
+		uniqdocs[docids[k]]=1; \
+	for (uniqdoc in uniqdocs) \
+		printf("%s\tQ0\t%s\t1\n", i, uniqdoc); \
+	delete uniqdocs; \
+} \
+}' equiv.txt $QREL > $QREL.merged
 
-trec_eval $MERGED_QREL $RESFILE
+trec_eval $QREL.merged $RESFILE
